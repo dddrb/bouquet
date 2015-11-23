456e071d2a834f3febb74c204c69e3f1824f2ec6
ogom <ogom@outlook.com>
2015-11-23 22:36:53 +0900

    Remove spec dummy

--- core/spec/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount Bouquet::Core::Engine => "/bouquet_core"
-end
--- gate/spec/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount Bouquet::Gate::Engine => "/bouquet_gate"
-end
