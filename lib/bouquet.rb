bf8e9f82580dc9a9a3dd7d3f0b0ccb7df5bfd536
ogom <ogom@outlook.com>
2016-01-20 21:30:59 +0900

    Update 0.2.1

--- lib/bouquet.rb
+++ lib/bouquet.rb
@@ -1,4 +1,7 @@
-require "bouquet/version"
+require 'bouquet/version'
+require 'bouquet-core'
+require 'bouquet-gate'
+require 'bouquet-store'
 
 module Bouquet
   # Your code goes here...
--- lib/bouquet/version.rb
+++ lib/bouquet/version.rb
@@ -1,3 +1,3 @@
 module Bouquet
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
